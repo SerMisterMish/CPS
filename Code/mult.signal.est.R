d2b817c93cb9a6719a66995d911fcd7bcbfec7aa
Nikita Khromov <hromovn@mail.ru>
2024-04-09 22:07:11 +0300

    Mandated edits

--- Code/mult.signal.est.R
+++ Code/mult.signal.est.R
@@ -4,91 +4,9 @@ library(purrr)
 library(Rssa)
 library(Metrics)
 
-tssa3 <- function(s, I = (length(s) + 2) %/% 3, L = I) {
-  X <- tens3(s, I, L)
-  result <- list()
-  result$X <- X
-  result$modes <- list(I = I, L = L, J = length(s) - I - L + 2)
-  result$hosvd <- rTensor::hosvd(X)
-  return(result)
-}
-
-# tens3.old <- function(s, I, L) {
-#   require("rTensor")
-#   v <- as.vector(s)
-#   N <- length(v)
-#   J <- N - I - L + 2
-#   X <- array(NA, c(I, L, J))
-#   for (i in 1:J) {
-#     X[, , i] <- outer(1:I, 1:L, function(x, y) s[i + x + y - 2])
-#   }
-#   as.tensor(X)
+# rcnorm <- function(n, mean = 0, var = 1) {
+#   sqrt(var / 2) * rnorm(n, mean = Re(mean)) + 1i * sqrt(var / 2) * rnorm(n, mean = Im(mean))
 # }
-
-tens3 <- function(s, I, L) {
-  require("rTensor")
-  v <- as.vector(s)
-  N <- length(v)
-  J <- N - I - L + 2
-  X <- sapply(1:J, function(j) Rssa::hankel(s[j:(j + I + L - 2)], I), simplify = "array")
-  as.tensor(X)
-}
-
-# diagonal averaging i + j + k = const, const = 3:(I+L+J)
-reconstruct.group3 <- function(X.tens) {
-  X <- X.tens@data
-  I <- length(X[, 1, 1])
-  L <- length(X[1, , 1])
-  J <- length(X[1, 1,])
-  s <- vector(mode = "numeric", length = I + L + J - 2)
-  for (C in 3:(I + L + J)) {
-    sum <- 0
-    count <- 0
-    for (i in 1:(C - 2)) {
-      for (l in 1:(C - 1 - i)) {
-        if (i <= I && l <= L && C - i - l <= J) {
-          sum <- sum + X[i, l, C - i - l]
-          count <- count + 1
-        }
-      }
-    }
-    s[C - 2] <- sum / count
-  }
-  return(s)
-}
-
-make.group <- function(hosvd, group = 1:min(hosvd$Z@modes)) {
-  ttl(hosvd$Z[group, group, group, drop = FALSE], list(
-    as.matrix(hosvd$U[[1]][, group]),
-    as.matrix(hosvd$U[[2]][, group]),
-    as.matrix(hosvd$U[[3]][, group])),
-      1:3)
-}
-
-make.groupHOOI <- function(X, group) {
-  r <- length(group)
-  hooi_x <- hooi(X@data, r = c(r, r, r), itermax = 1500, tol = 1.e-7)
-  G <- hooi_x$G
-  U <- hooi_x$U
-  ## Reconstruct the hooi approximation.
-  X_approx <- atrans(G, U)
-  as.tensor(X_approx)
-}
-
-t3.reconstruct <- function(p, groups) {
-  stopifnot(is.list(groups))
-  lapply(lapply(groups, make.group, hosvd = p$hosvd), reconstruct.group3)
-}
-
-t3.reconstructHOOI <- function(p, groups) {
-  require("tensr")
-  stopifnot(is.list(groups))
-  reconstruct.group3(make.groupHOOI(p$X, group = groups[[1]]))
-}
-
-rcnorm <- function(n, mean = 0, var = 1) {
-  sqrt(var / 2) * rnorm(n, mean = Re(mean)) + 1i * sqrt(var / 2) * rnorm(n, mean = Im(mean))
-}
 
 snr.to.sd <- function(signal, SNR) {
   sqrt(mean(abs(signal)^2) / 10^(SNR / 10))
@@ -110,23 +28,23 @@ c2 <- rnorm(Q)
 # w2 <- c(0.22, 0.24)
 # l1 <- sample(w1, Q, replace = TRUE)
 # l2 <- sample(w2, Q, replace = TRUE)
-s <- exp(-0.01 * 0:N) * cos(2 * pi * 0.2 * 0:N) %o% c1 + exp(-0.02 * 0:N) * cos(2 * pi * 0.22 * 0:N) %o% c2
-# s <- sapply(1:Q, function(i) exp(-0.01 * 0:N) * cos(2 * pi * 0.2 * 0:N + pi * i/ 6) * c1[i] +
-#                             exp(-0.02 * 0:N) * cos(2 * pi * 0.22 * 0:N + pi * i / 9) * c2[i])
+# s <- exp(-0.01 * 0:N) * cos(2 * pi * 0.2 * 0:N) %o% c1 + exp(-0.02 * 0:N) * cos(2 * pi * 0.22 * 0:N) %o% c2
+s <- sapply(1:Q, function(i) exp(-0.01 * 0:N) * cos(2 * pi * 0.2 * 0:N + pi * i/ 6) * c1[i] +
+                            exp(-0.02 * 0:N) * cos(2 * pi * 0.22 * 0:N + pi * i / 9) * c2[i])
 # s <- sapply(1:Q, function(i) exp(-0.01 * 0:N) * cos(2 * pi * l1[i] * 0:N) * c1[i] +
 #                             exp(-0.02 * 0:N) * cos(2 * pi * l2[i] * 0:N) * c2[i])
 # r <- 8
 r <- 4
 # r3 <- 8
-# r3 <- 4
-r3 <- 2
+r3 <- 4
+# r3 <- 2
 SNR <- 30
 # sigma <- mean(sapply(1:Q, function(i) snr.to.sd(s[, i], SNR)))
 sigma <- 0.02
 
 mult.signest.sc <- function(L, Mat = TRUE, Tens = TRUE) {
   # s.n <- s + rcnorm(N + 1, var = sigma^2)
-  s.n <- s + rnorm(N + 1, sd = sigma)
+  s.n <- s + rnorm((N + 1) * Q, sd = sigma)
 
   estimates <- array(dim = c(nrow(s), Q, 2))
 
@@ -149,7 +67,8 @@ mult.signest.sc <- function(L, Mat = TRUE, Tens = TRUE) {
 
 R <- 1000
 signal.tens <- s %o% rep(1, R)
-L.mat <- 17; L.tens <- 17
+# L.mat <- 22; L.tens <- 20 # - equal phases
+L.mat <- 21; L.tens <- 21 # - linear phases
 se.mult.comp.res <- list()
 system.time({
   set.seed(1)
